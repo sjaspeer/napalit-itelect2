871ef6fa0d7cd0c9b227416569095fb5ffbd174f
sjaspeer <snapalit_1400330@uic.edu.ph>
2017-07-28 12:25:20 +0800

    software exam

---
+++ server.js
@@ -0,0 +1,32 @@
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public')));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.get('/list', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/list.html'));
+});
+app.get('/Assignment2', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/Assignment2.html'));
+});
+app.get('/Assignment3', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/Assignment3.html'));
+});
+app.get('/Assignment4', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/Assignment4.html'));
+});
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+});
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
